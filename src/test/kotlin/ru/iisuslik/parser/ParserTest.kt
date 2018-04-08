7cdee1470f17b2cdfa2ae7c2b4c97761490710df
iisuslik43 <al.s.fedotov1998@yandex.ru>
2018-04-08 22:11:14 +0300

    More fix

--- src/test/kotlin/ru/iisuslik/parser/ParserTest.kt
+++ src/test/kotlin/ru/iisuslik/parser/ParserTest.kt
@@ -12,7 +12,7 @@ class ParserTest {
 
     @Test
     fun parsePlus() {
-        val(rest, node) = parse("2+2")
+        val (rest, node) = parse("2+2")
         assertEquals("", rest)
         assertEquals("(2+2)", node.toString())
         printAllAboutNode(node)
@@ -20,7 +20,7 @@ class ParserTest {
 
     @Test
     fun manyPlusMinus() {
-        val(rest, node) = parse("2+2-5-6+7")
+        val (rest, node) = parse("2+2-5-6+7")
         assertEquals("", rest)
         assertEquals("((((2+2)-5)-6)+7)", node.toString())
         assertEquals(0, node.calculate())
@@ -28,14 +28,14 @@ class ParserTest {
 
     @Test
     fun branchesInTheMiddle() {
-        val(rest, node) = parse("1+(2-3)-4")
+        val (rest, node) = parse("1+(2-3)-4")
         assertEquals("", rest)
         assertEquals(-4, node.calculate())
     }
 
     @Test
     fun allOperations() {
-        val(rest, node) = parse("1+2*3-4/2+2*2^2")
+        val (rest, node) = parse("1+2*3-4/2+2*2^2")
         assertEquals("", rest)
         assertEquals(13, node.calculate())
         printAllAboutNode(node)
@@ -43,7 +43,7 @@ class ParserTest {
 
     @Test
     fun multiply() {
-        val(rest, node) = parse("1*2+3")
+        val (rest, node) = parse("1*2+3")
         assertEquals("", rest)
         assertEquals(5, node.calculate())
         printAllAboutNode(node)
@@ -51,7 +51,7 @@ class ParserTest {
 
     @Test
     fun divide() {
-        val(rest, node) = parse("48/2/3+1")
+        val (rest, node) = parse("48/2/3+1")
         assertEquals("", rest)
         assertEquals(9, node.calculate())
         printAllAboutNode(node)
@@ -59,15 +59,15 @@ class ParserTest {
 
     @Test
     fun powerWorks() {
-        val(rest, node) = parse("3*2^2^3+1")
+        val (rest, node) = parse("3*2^2^3+1")
         assertEquals("", rest)
         assertEquals(769, node.calculate())
         printAllAboutNode(node)
     }
 
     @Test
-    fun harderBranches() {
-        val(rest, node) = parse("2^(3+7*(3-2-1))+1")
+    fun harderBrackets() {
+        val (rest, node) = parse("2^(3+7*(3-2-1))+1")
         assertEquals("", rest)
         assertEquals(9, node.calculate())
         printAllAboutNode(node)
@@ -75,52 +75,64 @@ class ParserTest {
 
     @Test(expected = ParserException::class)
     fun emptyString() {
-        val(rest, node) = parse("")
+        val (rest, node) = parse("")
         assertEquals("", rest)
         printAllAboutNode(node)
     }
 
     @Test(expected = ParserException::class)
-    fun strangeBranches() {
-        val(rest, node) = parse("2+()-2")
+    fun strangeBrackets() {
+        val (rest, node) = parse("2+()-2")
         assertEquals("", rest)
         printAllAboutNode(node)
     }
 
     @Test(expected = ParserException::class)
-    fun wrongSymbol() {
-        val(rest, node) = parse("2&2-2")
+    fun wrongSymbolExceptionHappened() {
+        val (rest, node) = parse("2&2-2")
         assertEquals("", rest)
         printAllAboutNode(node)
     }
 
     @Test
+    fun wrongSymbolPosition() {
+        var happend = false
+        try {
+            parse("2&2-2")
+        } catch (e: ParserException) {
+            happend = true
+            assertEquals(4, e.restLength)
+        }
+        assertTrue(happend)
+    }
+
+    @Test
     fun bigNumbers() {
-        val(rest, node) = parse("434343+43411-123124")
+        val (rest, node) = parse("434343+43411-123124")
         assertEquals("", rest)
         assertEquals(354630, node.calculate())
         printAllAboutNode(node)
     }
 
     @Test
-    fun branchesTest1() {
-        val(rest, node) = parse("(2+2)*2")
+    fun bracketTest1() {
+        val (rest, node) = parse("(2+2)*2")
         assertEquals("", rest)
         assertEquals(8, node.calculate())
         printAllAboutNode(node)
     }
 
     @Test
-    fun branchesTest2() {
-        val(rest, node) = parse("(2+2)*(2+3)")
+    fun bracketTest2() {
+        val (rest, node) = parse("(2+2)*(2+3)")
         assertEquals("", rest)
         assertEquals(20, node.calculate())
         printAllAboutNode(node)
     }
 
     @Test
-    fun uselessBranches() {
-        val(rest, node) = parse("(2+2)")
+    fun uselessBrackets() {
+        val (rest, node) = parse("(2+2)")
         assertEquals("", rest)
         assertEquals(4, node.calculate())
         printAllAboutNode(node)
@@ -129,5 +141,14 @@ class ParserTest {
     @Test
     fun fromTestFile() {
         parseFromFile("./src/test/resources/testFile")
+    }
+
+    @Test
+    fun manyBrackets() {
+        val (rest, node) = parse("(((((2+3)))))")
+        assertEquals("", rest)
+        assertEquals(5, node.calculate())
+        assertEquals("(2+3)", node.toString())
+        printAllAboutNode(node)
     }
 }
